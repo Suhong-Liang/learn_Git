7f125f88d740776676f7bb3c966ec402adeeb2aa
Suhong Liang <suhongliang@Suhongs-MacBook-Pro.local>
2019-11-03 15:47:15 -0500

    This is a combination of changes

--- hello.py
+++ hello.py
@@ -4,4 +4,5 @@ name = myname.get_name()
 print('hello {}'.format(name))
 
 print("renjie is making some change")
-print("renjie is  making  second some change")
+print("renjie is  making  second change")
+print("suhong is  making  second change")
